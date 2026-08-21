314710c32d4073ef52938ff8440c0f758fd52abb
antonforniniMP-eng <antonforniniMP@icloud.com>
2026-08-21 13:43:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const SUPABASE_ANON_KEY = "sb_publishable_gD5PkCtUOXnWdp6WKgYPWQ_rtlvJc3x";
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Doit être IDENTIQUE au mot de passe mis dans schema.sql (change-moi-123)
-const ADMIN_PASSWORD = "change-moi-123";
+const ADMIN_PASSWORD = "Mdp4nt0n1603";
